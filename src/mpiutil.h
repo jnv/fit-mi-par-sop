0a520ebed9f2a36ab1bf3e3e429f62431ee43dd9
Jan Vlnas <git@jan.vlnas.cz>
2012-11-11 17:47:59 +0100

    Prep Par, poormans logger

---
+++ src/mpiutil.h
@@ -0,0 +1,10 @@
+#ifndef MPIUTIL_H_
+#define MPIUTIL_H_
+
+#include "mpi.h"
+
+extern int _thisRank;
+extern int _procCnt;
+
+
+#endif // MPIUTIL_H_
--- src/par.cpp
+++ src/par.cpp
@@ -1,42 +1,150 @@
 /*
  ============================================================================
- Name        : par.c
- Author      : 
- Version     :
- Copyright   : 
- Description : Compute Pi in MPI C++
+ Name        : MI-PAR-SOP Paralelni reseni
+ Author      :
+ Description :
  ============================================================================
  */
-#include <math.h> 
-#include "mpi.h" 
+#include <cstdlib>
 #include <iostream>
+#include <stack>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <sys/resource.h>
+#include "common.h"
+#include "mpiutil.h"
+#include "Node.h"
+#include "Log.h"
 using namespace std;
- 
-int main(int argc, char *argv[]) {
-	int n, rank, size, i;
-	double PI25DT = 3.141592653589793238462643;
-	double mypi, pi, h, sum, x;
 
-	MPI::Init(argc, argv);
-	size = MPI::COMM_WORLD.Get_size();
-	rank = MPI::COMM_WORLD.Get_rank();
+int n = 0, c = 0, a = 0;
+int _upperBound = 0;
+int * _inputSet; // pole vstupnich dat
 
-	n=1000; // number of intervals
+int _thisRank;
+int _procCnt;
 
-	MPI::COMM_WORLD.Bcast(&n, 1, MPI::INT, 0);
-	h = 1.0 / (double) n;
-	sum = 0.0;
-	for (i = rank + 1; i <= n; i += size) {
-		x = h * ((double) i - 0.5);
-		sum += (4.0 / (1.0 + x * x));
-	}
-	mypi = h * sum;
+/**
+ * Main solve cycle
+ */
+void doSolve()
+{
 
-	MPI::COMM_WORLD.Reduce(&mypi, &pi, 1, MPI::DOUBLE, MPI::SUM, 0);
-	if (rank == 0)
-		cout << "pi is approximately " << pi << ", Error is "
-				<< fabs(pi - PI25DT) << endl;
-
-	MPI::Finalize();
-	return 0;
 }
+
+bool loadSet(char * fname)
+{
+	ifstream f;
+	f.open(fname);
+
+	if (!f.is_open())
+	{
+		cout << "Cannot open input file: " << fname << endl;
+		return false;
+	}
+
+	f >> n;
+#ifndef DEBUG
+	if(n < 20)
+	{
+		cout << "n must be 20 or more";
+		f.close();
+		return false;
+	}
+#endif
+
+	f >> c;
+	f >> a;
+#ifndef DEBUG
+	if(a < 2 || a > n / 10)
+	{
+		cout << "a must be more than 1 and less than n/10.";
+		f.close();
+		return 0;
+	}
+#endif
+
+	cout << "c (max price per subset): " << c << endl
+			<< "a (number of subsets): " << a << endl;
+
+	_inputSet = new int[n];
+
+	cout << "Loading " << n << " numbers" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		int in;
+		f >> in;
+		_inputSet[i] = in;
+	}
+
+	cout << "Loaded: ";
+	for (int i = 0; i < n; i++)
+	{
+		cout << _inputSet[i] << " ";
+	}
+	cout << endl;
+
+	_upperBound = a * (c - 1);
+	cout << "Upper bound is: " << _upperBound << endl;
+
+	return true;
+}
+
+void initFirstProc(char * fname)
+{
+
+}
+
+void initOtherProc()
+{
+
+}
+
+int main(int argc, char ** argv)
+{
+	double start, stop;
+
+	MPI_Init(&argc, &argv); /* start up MPI */
+	MPI_Comm_rank(MPI_COMM_WORLD, &_thisRank);
+	MPI_Comm_size(MPI_COMM_WORLD, &_procCnt);
+
+	char* logFile;
+	sprintf(logFile, "log-%d.log", _thisRank);
+	Log::Init(logFile);
+
+	if(_thisRank == 0)
+	{
+		initFirstProc(argv[1]);
+	}
+	else
+	{
+		initOtherProc();
+	}
+
+	if (argc != 2)
+	{
+		cout << "Usage: par <file>" << endl;
+		return 0;
+	}
+
+
+
+	if (!loadSet(argv[1]))
+	{
+		return 1;
+	}
+
+	start = MPI_Wtime();
+	doSolve();
+	stop = MPI_Wtime();
+	delete[] _inputSet;
+
+	cout << "Solve time: " << stop - start << endl;
+
+	return 0;
+
+}
+
