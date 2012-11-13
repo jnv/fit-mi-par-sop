4156711a3b1a776ccf31f44e1ac49eb56b01588f
Jan Vlnas <git@jan.vlnas.cz>
2012-11-13 22:09:47 +0100

    Rm unneeded files

--- src/Log.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * Log.cpp
- *
- *  Created on: 11.11.2012
- *      Author: j
- */
-
-#include "Log.h"
-
--- src/Node.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * Node.cpp
- *
- *  Created on: 9.11.2012
- *      Author: j
- */
-
-#include "Node.h"
-
