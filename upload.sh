9d547f80b79242fd32dd7efdea3a304a682357dd
Jan Vlnas <git@jan.vlnas.cz>
2012-11-13 13:40:39 +0100

    delete files on upload

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 SSH_HOST=vlnasjan@star.fit.cvut.cz #or "username@host"
 
-rsync -rlvze 'ssh' --exclude-from 'rsync-exclude' . $SSH_HOST:~/par
+rsync -rlvze 'ssh' --delete --exclude-from 'rsync-exclude' . $SSH_HOST:~/par
